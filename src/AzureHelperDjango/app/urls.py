578927f40536fdf1630e4207748739e24fe859cb
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-06-24 11:20:34 +0200

    feat : made chat bot persistent within session

--- src/AzureHelperDjango/app/urls.py
+++ src/AzureHelperDjango/app/urls.py
@@ -1,8 +1,9 @@
 # from .views import IndexView, set_affluence, AccountingView, PredictionView
 from django.views.generic import TemplateView
 from django.urls import path
+from .views import ChatView
 
 urlpatterns = [
     path("home", TemplateView.as_view(template_name="app/home.html"), name="home"),
-    path("chat", TemplateView.as_view(template_name="app/chat.html"), name="chat"),
+    path("chat", ChatView.as_view(template_name="app/chat.html"), name="chat"),
 ]
